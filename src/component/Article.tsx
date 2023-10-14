4a5bf75545af25869cf35a109a66edaba7e0b575
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-14 19:23:53 +0900

    Article 컴포넌트의 TitleBox컴포넌트 css 작성

--- src/component/Article.tsx
+++ src/component/Article.tsx
@@ -5,12 +5,16 @@ function Article() {
 
   return (
     <ArticleBox>
+			
 			<TitleBox>
 				<Title>
 					국방부 “北, 화성-17 실패 만회하려 영상 짜깁기… 성공 조작”
 				</Title>
-				<Icon src={substract}/>
+				<IconBox>
+					<Icon src={substract}/>
+				</IconBox>
 			</TitleBox>
+
 			<SourceData>
 			</SourceData>
     </ArticleBox>
@@ -34,19 +38,31 @@ const ArticleBox = styled.div`
 const TitleBox = styled.div`
 	width: 295px;
 	height: 56px;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const Title = styled.div`
 	width: 260px;
 	height: 56px;
+	color: #000000;
 	font-size: 18px;
 	font-weight: 600;
 	line-height: 28px;
+	letter-spacing: -0.05em;
+`;
+
+
+const IconBox = styled.div`
+	width: 24px;
+	height: 24px;
+	padding: 4px;
+	gap: 10px;
 `;
 
 const Icon = styled.img`
 	width: 16px;
-	height: 15px;
+	height: 16px;
 `;
 
 const SourceData = styled.div`
