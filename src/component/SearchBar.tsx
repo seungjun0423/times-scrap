a3765805ec4c511075e98ce288015d70f1d6b7a3
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-14 17:29:13 +0900

    SearchInput.tsx css작성

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
-
+import SearchInput from "./ui/SearchInput";
 function SearchBar() {
 
   return (
     <SearchBox>
+			<UiBox>
+				<SearchInput />
+			</UiBox>
     </SearchBox>
   )
 }
@@ -11,8 +14,16 @@ function SearchBar() {
 export default SearchBar;
 
 const SearchBox = styled.div`
-	width: 375px;
+	width: 100%;
 	height: 60px;
 	background-color: #FFFFFF;
 	border-bottom: 1px solid #C4C4C4;
+`;
+
+const UiBox = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	padding-left: 20px;
 `;
--- src/component/ui/SearchInput.tsx
+++ src/component/ui/SearchInput.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import search from "@/assets/svg/search.svg";
 
 function SearchInput() {
 
   return (
     <SearchInputBox>
+			<Icon src={search}/>
+			<Input placeholder="전체 헤드라인">
+			</Input>
     </SearchInputBox>
   )
 }
@@ -11,5 +15,27 @@ function SearchInput() {
 export default SearchInput;
 
 const SearchInputBox = styled.div`
+	width: auto;
+	height: auto;
+	display: flex;
+	align-items: center;
+	padding-top: 6px;
+	padding-right: 12px;
+	padding-bottom: 4px;
+	padding-left: 12px;
+	border: 1px solid #C4C4C4;
+	border-radius: 30px;
+	gap: 4px;
+`;
 
+const Icon = styled.img`
+	width: 16px;
+	height: 16px;
+`;
+
+const Input = styled.input`
+	width: 73px;
+	height: 24px;
+	border: none;
+	outline:none;
 `;
