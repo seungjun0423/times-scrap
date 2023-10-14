a5a8c4aad52c4a3466f22763870521506022453d
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-14 14:14:27 +0900

    절대 경로 사용을 위해 vite.config.ts 코드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@page', replacement: path.resolve(__dirname, 'src/page') },
+			{ find: '@component', replacement: path.resolve(__dirname, 'src/component') },
+			{ find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+			{ find: '@model', replacement: path.resolve(__dirname, 'src/model') },
+			{ find: '@style', replacement: path.resolve(__dirname, 'src/style') },
+    ]
+  }
 })
