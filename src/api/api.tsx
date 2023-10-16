fe01f6c304964035cfdc151b34d2f7713aeba6a8
이승준 <87581422+seungjun0423@users.noreply.github.com>
2023-10-16 16:44:35 +0900

    getTodayHeadline 함수 description 작성

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -9,6 +9,7 @@ const { VITE_API_URL, VITE_API_KEY } = import.meta.env;
 // 	return req;
 // };
 
+/**Parameter: (date: YYYY-MM-DD), 단일 파라미터 date 값을 인자로 받으며 YYYY-MM-DD 포맷의 형태로 받는다.   */
 const getTodayHeadline = () => {
 	const req = axios.get(
 		`${VITE_API_URL}fq=pub_date:(2023-10-14) AND Section Name Values:('Today's Headlines')&api-key=${VITE_API_KEY}`
